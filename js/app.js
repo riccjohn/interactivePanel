7a0a1afc9d3ec3a5f173cdb6a85ad014db80cb5a
John Riccardi <ricjohn@gmail.com>
2017-10-13 13:59:39 -0400

    gave new classes and IDs to videos, added js, but still has bugs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
+  // const video = document.getElementsByClassName("video");
+  const video1 = document.getElementById("video1");
+  const video2 = document.getElementById("video2");
+  const video3 = document.getElementById("video3");
+
+  // video.addEventListener('ended', returnToMenu, false);
+
+  video1.addEventListener('ended', returnToMenu, false);
+  video2.addEventListener('ended', returnToMenu, false);
+  video3.addEventListener('ended', returnToMenu, false);
+
+  function returnToMenu() {
+    window.location.href = 'index.html'
+  }
 });
