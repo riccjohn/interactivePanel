acf42e7d92d4b273cb6c999a4cf9130885cd8868
John Riccardi <ricjohn@gmail.com>
2017-10-18 19:17:04 -0400

    Checking if element exists, and then running my logic. Referencing same ID for each element

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
   // const video = document.getElementsByClassName("video");
-  const video1 = document.getElementById("video1");
-  const video2 = document.getElementById("video2");
-  const video3 = document.getElementById("video3");
+  const video = document.getElementById("video");
+
 
   // video.addEventListener('ended', returnToMenu, false);
-
-  video1.addEventListener('ended', returnToMenu, false);
-  video2.addEventListener('ended', returnToMenu, false);
-  video3.addEventListener('ended', returnToMenu, false);
+  if(video) {
+    video.addEventListener('ended', returnToMenu, false);
+  }
 
   function returnToMenu() {
     window.location.href = 'index.html'
